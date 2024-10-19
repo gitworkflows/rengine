39052f42cdf2eb441f8d9975f35e97b6c125209e
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-10-19 09:12:55 +0600

    Uncontrolled data used in path expression
    To fix the problem, we need to validate the path parameter to ensure it does not allow directory traversal and is contained within the settings.MEDIA_ROOT directory. We can achieve this by normalizing the path using os.path.normpath and then checking if the resulting path starts with settings.MEDIA_ROOT.
    
    Normalize the file_path using os.path.normpath.
    Check if the normalized file_path starts with settings.MEDIA_ROOT.
    If the check fails, raise an Http404 error.
    If the check passes, proceed with the existing logic.

--- web/reNgine/views.py
+++ web/reNgine/views.py
@@ -6,16 +6,16 @@ from django.conf import settings
 
 @login_required
 def serve_protected_media(request, path):
-    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    file_path = os.path.normpath(os.path.join(settings.MEDIA_ROOT, path))
+    if not file_path.startswith(settings.MEDIA_ROOT):
+        raise Http404("File not found")
     if os.path.isdir(file_path):
         raise Http404("File not found")
     if os.path.exists(file_path):
         content_type, _ = mimetypes.guess_type(file_path)
         response = HttpResponse()
-        # response['Content-Disposition'] = f'attachment; filename={os.path.basename(file_path)}'
         response['Content-Type'] = content_type
         response['X-Accel-Redirect'] = f'/protected_media/{path}'
         return response
     else:
         raise Http404("File not found")
-
